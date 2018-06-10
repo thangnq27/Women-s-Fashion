0c89e7b1fb7cea77f8c51c84bc7366b0234cc101
thangnq27 <nqthang27@gmail.com>
2018-06-11 00:12:31 +0700

    api

--- OnlineShopAPI/Controllers/ProductCategoryController.cs
+++ OnlineShopAPI/Controllers/ProductCategoryController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Web.Http;
 using Model.EF;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OnlineShopAPI.Controllers
 {
@@ -16,21 +20,53 @@ namespace OnlineShopAPI.Controllers
         public string GetAllCategories()
         {
             //return db.ProductCategories.AsEnumerable();
-            
-            string result = "[";
-            foreach (ProductCategory p in db.ProductCategories)
-            {
-                if(p.ParentID==null)
-                    result += "{\"id\":\"" + p.ID + "\",\"parentname\":\"" + p.Name + "\",\"childname\":\"" + string.Empty + "\"},";
-                else
-                {
-                    result += "{\"id\":\"" + p.ID + "\",\"parentname\":\"" + string.Empty + "\",\"childname\":\"" + p.Name + "\"},";
-                }
-            }
 
+            //string result = "[";
             
-            result = result.TrimEnd(',');
-            result = result + "]";
+            Hashtable hashCategory = new Hashtable();
+
+            //Dictionary<string,List<ProductCategory>> dicCategory=new Dictionary<string, List<ProductCategory>>();
+            Dictionary<string, string> dicCategory = new Dictionary<string, string>();
+            string result = "{";
+            foreach (ProductCategory p in db.ProductCategories.Where(r=>r.ParentID==null))
+            {
+                //if (p.ParentID == null)
+                //{
+                //    //hashCategory.Add(p.ID,p.ID);
+                //    result += "{\"id\":\"" + p.ID + "\",\"parentname\":\"" + p.Name + "\",\"childname\":\"" + string.Empty + "\"},";
+                //}
+                //else
+                //{
+                //    //hashCategory.Add(p.ParentID,p.ID);
+                //    result += "{\"id\":\"" + p.ID + "\",\"parentname\":\"" + string.Empty + "\",\"childname\":\"" + p.Name + "\"},";
+                //}
+
+                //foreach (ProductCategory child in db.ProductCategories.Where(r => r.ParentID == p.ID))
+                //{
+                //    //result += "{\"idParent\":\"" + p.ID + "\",\"parentname\":\"" + p.Name + "\",\"idChild\":\"" + child.ID + "\",\"childname\":\"" + child.Name + "\"},";
+                //}
+                //string key = p.ID + ";#" + p.Name;
+                string key = "\""+p.Name+"\":[";
+                string tmp = "";
+                IEnumerable<ProductCategory> child = db.ProductCategories.Where(r => r.ParentID == p.ID);
+                //dicCategory.Add(key, new List<ProductCategory>(child));
+                
+                foreach (ProductCategory x in child)
+                {
+                    tmp += "{\"id\":\"" + x.ID + "\",\"name\":\"" + x.Name + "\"},";
+                    //tmp += "{id:" + x.ID + ",name:" + x.Name + "},";
+                }
+                tmp = tmp.TrimEnd(',')+"],";
+                //tmp = tmp + "}";
+                dicCategory.Add(key, tmp);
+                result += key + tmp;
+
+            }
+            result = result.TrimEnd(',')+"}";
+            //string result = JsonConvert.SerializeObject(dicCategory, Formatting.Indented);
+            //result = result.TrimEnd(',');
+            //result = result + "]";
+
             return result;
         }
     }
